bf3568225753d7920716d8add5526241945a22e7
Denzelllll <denzelsun01@gmail.com>
2024-10-07 15:14:03 -0400

    1

--- HW04A/sketch.js
+++ HW04A/sketch.js
@@ -1,25 +1,19 @@
-let r = 15
+let r = 15;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255, 38, 0);
- noStroke();
-  
-  for (let n = 0; n < windowWidth; n+=30) {
-  
-    for (let d = 0; d < windowWidth; d+=30) {
-  
-      let c=random(["#0f0729","#0f0729","#a3003c","#0f0729"]);
+  noStroke();
+
+  for (let n = 0; n < windowWidth; n += 30) {
+    for (let d = 0; d < windowWidth; d += 30) {
+      let c = random(["#0f0729", "#0f0729", "#a3003c", "#0f0729"]);
       fill(c);
-      let ab= dist(n,d,windowWidth/2,windowHeight/2);
-      let l = map(ab,0,dist(0,0,windowWidth/2,windowHeight/2),0,40);
-      let ran1=random(-r, r);
-      ellipse(n+ran1,d+ran1,l+ran1);
-     
+      let ab = dist(n, d, windowWidth / 2, windowHeight / 2);
+      let l = map(ab, 0, dist(0, 0, windowWidth / 2, windowHeight / 2), 0, 40);
+      let ran1 = random(-r, r);
+      ellipse(n + ran1, d + ran1, l + ran1);
     }
   }
-  
 }
 
-function draw() {
- 
-}
+function draw() {}
--- HW04B/sketch.js
+++ HW04B/sketch.js
@@ -1,48 +1,43 @@
 /*
 " "title" : "Wall Drawing #46 (1970)",
     "description" : "Vertical lines, not straight, not touching, covering the wall evenly.""*/
-let r1=20
-let r2=20
-let r3=20
-let r4=20
+let r1 = 20;
+let r2 = 20;
+let r3 = 20;
+let r4 = 20;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255, 38, 0);
-  noFill(); // Do not fill shapes
+  noFill();
   stroke(0);
-  strokeWeight(4); // Set stroke color to black
-  for (let n = 0; n < windowWidth; n+=windowWidth/70) {
-    randomlineV(n,windowHeight/9);
-    
+  strokeWeight(4);
+  for (let n = 0; n < windowWidth; n += windowWidth / 70) {
+    randomlineV(n, windowHeight / 9);
   }
 }
- function randomlineV(a,b){
-  let ran1=random(-r1,r1);
-  let ran2=random(-r2,r2);
-  let ran3=random(-r3,r3);  
-  let ran4=random(-r4,r4);
-  let ran5=random(-r1,r1);
-  let ran6=random(-r2,r2);
-  let ran7=random(-r3,r3);  
-  let ran8=random(-r4,r4);
-  let ran9=random(-r1,r1);  
-  let ran10=random(-r2,r2);
-  
-  let l=random(470,480);
+function randomlineV(a, b) {
+  let ran1 = random(-r1, r1);
+  let ran2 = random(-r2, r2);
+  let ran3 = random(-r3, r3);
+  let ran4 = random(-r4, r4);
+  let ran5 = random(-r1, r1);
+  let ran6 = random(-r2, r2);
+  let ran7 = random(-r3, r3);
+  let ran8 = random(-r4, r4);
+  let ran9 = random(-r1, r1);
+  let ran10 = random(-r2, r2);
 
-  let c=random(["#0f0729","#a3003c","#0f0729"]);
+  let l = random(470, 480);
+
+  let c = random(["#0f0729", "#a3003c", "#0f0729"]);
   stroke(c);
-  line(a, ran1, a+ran2, b+ran2);
-  line( a+ran2, b+ran2, a+ran3, 2*b+ran3);
-  line( a+ran3, 2*b+ran3,a+ran4,3*b+ran4 );
-  line( a+ran4,3*b+ran4, a+ran5, 4*b+ran5);
-  line( a+ran5, 4*b+ran5,a+ran6,5*b+ran6 );
-  line( a+ran6, 5*b+ran6,a+ran7,6*b+ran7 );
-  line( a+ran7, 6*b+ran7,a+ran8,7*b+ran8 );
-  line( a+ran8, 7*b+ran8,a+ran9,8*b+ran9 );
-  line( a+ran9, 8*b+ran9,a+ran10,windowHeight);
-  
-
-
- }
-
+  line(a, ran1, a + ran2, b + ran2);
+  line(a + ran2, b + ran2, a + ran3, 2 * b + ran3);
+  line(a + ran3, 2 * b + ran3, a + ran4, 3 * b + ran4);
+  line(a + ran4, 3 * b + ran4, a + ran5, 4 * b + ran5);
+  line(a + ran5, 4 * b + ran5, a + ran6, 5 * b + ran6);
+  line(a + ran6, 5 * b + ran6, a + ran7, 6 * b + ran7);
+  line(a + ran7, 6 * b + ran7, a + ran8, 7 * b + ran8);
+  line(a + ran8, 7 * b + ran8, a + ran9, 8 * b + ran9);
+  line(a + ran9, 8 * b + ran9, a + ran10, windowHeight);
+}
